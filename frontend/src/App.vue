fb736ae964a86cdc16b832121bbf62683b760914
Saadansari234 <saadansari3212@gmail.com>
2025-04-20 18:30:35 +0530

    dd

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+  ssss
+</template>
+
+<script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        message: ''
+      }
+    },
+    mounted() {
+      axios.get('http://127.0.0.1:8000/app/')
+        .then(response => {
+          this.message = response.data.message
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }
+  </script>
